0c35c7dfede913980f7271bff80eb385e2d6e80b
Reinhardt Erasmus <reinhardt@methoddata.com>
2024-07-22 19:11:46 +0200

    Remove unnecessary imports of FontAwesomeIcon and faQuestionCircle from App.js

--- prompt-generator/src/App.js
+++ prompt-generator/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import PromptForm from './PromptForm';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
